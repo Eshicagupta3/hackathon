bd2838bef8a9025caa6c0703cd53bb7049760532
ishika <Eshicagupta3@gmail.com>
2023-08-24 00:14:19 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,18 +2,18 @@ const hre = require("hardhat");
 const fs = require('fs');
 const address = "0xF7079308daA87f8790679d9D7f9806c31EE17611";
 async function main() {
-  // const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  // const nftMarketplace = await NFTMarketplace.deploy();
-  // await nftMarketplace.deployed();
-  // console.log("nftMarketplace deployed to:", nftMarketplace.address);
-  console.log("nftMarketplace deployed to:", address);
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
+  await nftMarketplace.deployed();
+  console.log("nftMarketplace deployed to:", nftMarketplace.address);
+  // console.log("nftMarketplace deployed to:", address);
 
-  // fs.writeFileSync('./config.js', `
-  // export const marketplaceAddress = "${nftMarketplace.address}"
-  // `)
   fs.writeFileSync('./config.js', `
-  export const marketplaceAddress = "${address}"
+  export const marketplaceAddress = "${nftMarketplace.address}"
   `)
+  // fs.writeFileSync('./config.js', `
+  // export const marketplaceAddress = "${address}"
+  // `)
 }
 
 main()
